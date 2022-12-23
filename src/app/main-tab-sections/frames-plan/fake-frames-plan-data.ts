da55043abb34766bb49b5abdcbbcd76a9bf83cb9
richard <richard@themcdonalds.net>
2022-12-23 14:19:06 -0500

    Make up fake data for the frame plan

---
+++ src/app/main-tab-sections/frames-plan/fake-frames-plan-data.ts
@@ -0,0 +1,68 @@
+const fakeFramesPlanData: FramePlan = {
+  frameSets: [
+    {
+      frameSpec: {
+        frameType: DarkFrameType.biasFrame,
+        binning: 1,
+        exposure: 0
+      },
+      numberWanted: 16,
+      numberCaptured: 16
+    },
+    {
+      frameSpec: {
+        frameType: DarkFrameType.biasFrame,
+        binning: 2,
+        exposure: 0
+      },
+      numberWanted: 16,
+      numberCaptured: 16
+    },
+    {
+      frameSpec: {
+        frameType: DarkFrameType.biasFrame,
+        binning: 4,
+        exposure: 0
+      },
+      numberWanted: 16,
+      numberCaptured: 6
+    },
+    {
+      frameSpec: {
+        frameType: DarkFrameType.darkFrame,
+        binning: 1,
+        exposure: 60
+      },
+      numberWanted: 16,
+      numberCaptured: 0
+    },
+    {
+      frameSpec: {
+        frameType: DarkFrameType.darkFrame,
+        binning: 1,
+        exposure: 180
+      },
+      numberWanted: 16,
+      numberCaptured: 0
+    },
+    {
+      frameSpec: {
+        frameType: DarkFrameType.darkFrame,
+        binning: 1,
+        exposure: 300
+      },
+      numberWanted: 16,
+      numberCaptured: 0
+    },
+    {
+      frameSpec: {
+        frameType: DarkFrameType.darkFrame,
+        binning: 2,
+        exposure: 300
+      },
+      numberWanted: 16,
+      numberCaptured: 0
+    },
+  ],
+  currentSet: 2
+}
