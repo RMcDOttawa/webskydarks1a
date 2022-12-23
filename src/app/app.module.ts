07b403eff4b5ce545e09aa2f1b347a92200f8d7f
richard <richard@themcdonalds.net>
2022-12-23 10:43:20 -0500

    Create top app bar with logo and help button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+//  Angular
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//  App business logic
+
+//  Material
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppToolbarComponent
   ],
   imports: [
+    //  Angular
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    //  Material
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
