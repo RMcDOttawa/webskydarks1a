6dbb46339a1bbbaaa0cccb19034371f0b0343a32
richard <richard@themcdonalds.net>
2023-01-04 17:00:13 -0500

    Run Session tab: fill acquisition list table from saved frame plan.

--- src/app/main-tab-sections/run-session/acquisition-list/acquisition-list.component.ts
+++ src/app/main-tab-sections/run-session/acquisition-list/acquisition-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FramePlanService} from "../../../services/frame-plan/frame-plan.service";
 
 @Component({
   selector: 'app-acquisition-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcquisitionListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public framePlanService: FramePlanService) { }
 
   ngOnInit(): void {
   }
--- src/app/main-tab-sections/run-session/run-session.component.spec.ts
+++ src/app/main-tab-sections/run-session/run-session.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RunSessionComponent } from './run-session.component';
+import {MockComponent, MockDirective} from "ng-mocks";
+import {MatCardActions} from "@angular/material/card";
+import {CameraAutosaveComponent} from "./camera-autosave/camera-autosave.component";
+import {ConsoleLogComponent} from "./console-log/console-log.component";
+import {AcquisitionListComponent} from "./acquisition-list/acquisition-list.component";
 
 describe('RunSessionComponent', () => {
   let component: RunSessionComponent;
@@ -8,7 +13,15 @@ describe('RunSessionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RunSessionComponent ]
+      declarations: [
+        RunSessionComponent,
+        MockDirective(MatCardActions),
+      ],
+      imports: [
+        MockComponent(CameraAutosaveComponent),
+        MockComponent(ConsoleLogComponent),
+        MockComponent(AcquisitionListComponent),
+      ]
     })
     .compileComponents();
 
