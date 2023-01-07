d401ebc4b402657859149312b6b023884d377cd0
richard <richard@themcdonalds.net>
2023-01-07 14:20:40 -0500

    Improved "scroll to bottom" by moving scroll to after-view-checked method and eliminating kludge timer

--- src/app/main-tab-sections/run-session/console-log/console-log.component.ts
+++ src/app/main-tab-sections/run-session/console-log/console-log.component.ts
@@ -14,34 +14,24 @@ export class ConsoleLogComponent implements OnInit {
   //  Must use an event handler so the scrolling takes place after the new lines are added
   @ViewChild('console') private consoleDiv!: ElementRef;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
-
   //  Detect when the console text has changed and scroll the div to keep the last line visible
-  // ngOnChanges(changes: SimpleChanges) {
-    ngOnChanges() {
-    // console.log('  ConsoleLogComponent/ngOnChanges fired: ', changes);
-    if (this.consoleDiv) {
-      // console.log('   Scrolling div to bottom');
-      // console.log(`   scrollHeight = ${this.consoleDiv.nativeElement.scrollHeight}`);
-
-      //  Do the scroll after a fraction-second delay, so there is time for the added line to render
-      //  This was arrived at through experimentation.  Without the delay, the scroll only goes down to the
-      //  second-last line. Eventually I figured out that I was getting this change notification before the
-      //  div content had absorbed the new data and re-rendered.  There is probably a better way to do this
-      //  scroll after the rendering event rather than relying on a silly timer, but I haven't found it.
-      setTimeout(() => {
-        this.consoleDiv.nativeElement.scroll({
-          top: this.consoleDiv.nativeElement.scrollHeight,
-          bottom: 0,
-          left: 0,
-          behavior: 'smooth'
-        });
-      }, 100);
-    }
+  //  Do the scroll here because this method fires after the updated pane is rendered.
+  ngAfterViewChecked() {
+    // console.log('ConsoleLogComponent/ngAfterViewChecked entered');
+    this.consoleDiv.nativeElement.scroll({
+      top: this.consoleDiv.nativeElement.scrollHeight,
+      bottom: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
   }
+
+
 
 }
